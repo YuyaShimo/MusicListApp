910b1d109374a63c2116e58a78fa2df6d0a7ae42
YuyaShimo <mei300225@gmail.com>
2020-04-10 22:48:14 +0900

    LoginViewControllerファイルの生成

---
+++ MusicListApp/Controller/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  MusicListApp
+//
+//  Created by 下新原佑哉 on 2020/04/10.
+//  Copyright © 2020 Yuya shimoshimbara. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
